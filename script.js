3e7117c560f2d039519cce5ba28c26c61d04c6ad
vikas kumar <vk.vikas3003@gmail.com>
2023-05-22 11:42:50 +0530

    leaflet map added

--- script.js
+++ script.js
@@ -27,7 +27,18 @@ navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert("could not get your location");
